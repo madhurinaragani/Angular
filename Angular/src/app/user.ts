a3b1784df50b0325ab46577c97b8c6e22a905cfc
Praveen Jogi <praveen.jogi@nokia.com>
2020-10-01 23:32:05 -0700

    initial commit

---
+++ Angular/src/app/user.ts
@@ -0,0 +1,9 @@
+export class User{
+    constructor(
+        name:string,
+        status:string
+        //email:string,
+        //experience:number,
+        //domain:string
+    ){}
+}
---
+++ registration-service/src/main/java/com/reg/service/api/model/User.java
@@ -0,0 +1,48 @@
+package com.reg.service.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name="USER_REG_TBL")
+public class User {
+    
+	
+	@Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String status;
+	//private String email;
+   // private int experience;
+   // private String domain;
+    public int getId() {
+		return id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
